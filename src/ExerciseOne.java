9d7e54ab7de4f76cd29e3269cf1102ae8d5c264f
DanielaBeraja <danielaberaja@hotmail.com>
2020-07-24 16:08:58 -0300

    Exercise one and exercise two

---
+++ src/ExerciseOne.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ExerciseOne {
+    public static void main(String[] args) {
+        int height, base;
+        int area;
+
+        Scanner keyboard= new Scanner(System.in);
+        System.out.println("Ingrese la altura del triángulo: ");
+        height= keyboard.nextInt();
+        System.out.println("Ingrese la base del triángulo: ");
+        base= keyboard.nextInt();
+        area= (base * height)/2;
+        System.out.println("El area del triángulo es: "+ area);
+    }
+}
---
+++ src/ExerciseTwo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ExerciseTwo {
+    public static void main(String[] args) {
+
+        double pesoMexicano;
+        double dolarFinal;
+
+        Scanner keyboard= new Scanner(System.in);
+
+        System.out.println("Ingrese la cantidad de pesos Mexicanos a convertir: ");
+        pesoMexicano= keyboard.nextDouble();
+        dolarFinal= pesoMexicano/22.37;
+        System.out.println("La conversion a dolares es: USD"+ dolarFinal);
+
+    }
+}
